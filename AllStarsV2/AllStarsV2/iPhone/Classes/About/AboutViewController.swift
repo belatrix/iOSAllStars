36472be1ffa94439900842b5e836400cae1fc4a6
Javier Siancas <jsiancas@belatrixsf.com>
2017-10-11 14:47:17 -0500

    Javier-feat(ver tutorial): - Se agregaron nuevas imágenes.
    Javier-feat(ver "about"):
    - Se implementó efecto para que la altura del "header view" cambie cuando se mueva la propiedad "contentOffset" del UIScrollView.
    - Se implementó efecto para que la ubicación del "title label" cambie cuando se mueva la propiedad "contentOffset" del UIScrollView.
    - Se implementó efecto para que el tamaño de la fuente del "title label" cambie cuando se mueva la propiedad "contentOffset" del UIScrollView.

--- AllStarsV2/AllStarsV2/iPhone/Classes/About/AboutViewController.swift
+++ AllStarsV2/AllStarsV2/iPhone/Classes/About/AboutViewController.swift
@@ -10,14 +10,108 @@ import UIKit
 
 class AboutViewController: SWFrontGenericoViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet fileprivate weak var headerViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var scrollViewContentTopConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var titleLabelLeftConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var titleLabelBottomConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var menuButtonWidthConstraint: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var headerView: UIView!
+    @IBOutlet fileprivate weak var titleLabel: UILabel!
+    @IBOutlet private weak var aboutScrollView: UIScrollView!
+    
+    fileprivate var lastContentOffset: CGPoint = CGPoint.zero
+    fileprivate var maxHeightForHeaderView: CGFloat = 0.0
+    fileprivate var maxLeftForTitleLabel: CGFloat = 0.0
+    fileprivate var maxBottomForTitleLabel: CGFloat = 0.0
+    fileprivate var maxTitleLabelFontSize: CGFloat = 0.0
+    fileprivate var minHeightForHeaderView: CGFloat = 64.0
+    fileprivate var minLeftForTitleLabel: CGFloat = 0.0
+    fileprivate var minBottomForTitleLabel: CGFloat = 0.0
+    fileprivate var minTitleLabelFontSize: CGFloat = 19.0
+    
+    
+    
+    
+    
+    // MARK: - AboutViewController's methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Configuraciones adicionales.
+        self.maxHeightForHeaderView = self.headerViewHeightConstraint.constant /* El tamaño inicial de 'headerView' es lo máximo que puede aumentar. */
+        self.minLeftForTitleLabel   = self.titleLabelLeftConstraint.constant /*  */
+        self.maxLeftForTitleLabel   = self.menuButtonWidthConstraint.constant /*  */
+        self.maxBottomForTitleLabel = self.titleLabelBottomConstraint.constant /* */
+        self.maxTitleLabelFontSize  = self.titleLabel.font.pointSize
+        
+        self.headerView.layer.shadowOffset  = CGSize.zero
+        self.headerView.layer.shadowOpacity = 0.3
+        self.headerView.layer.shadowRadius  = 4.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+}
+
+
+
+
+
+extension AboutViewController: UIScrollViewDelegate {
+    
+    // MARK: - Definitions
+    
+    typealias RangeTuple = (value1: CGFloat, value2: CGFloat)
+    enum FunctionType: Int {
+        case increasing
+        case decreasing
+    }
+    
+    
+    
+    
+    
+    // MARK: - My own methods
+    
+    /**
+     */
+    private func valueBetween(yRange: RangeTuple, forX x: CGFloat, between xRange: RangeTuple, functionType type: FunctionType) -> CGFloat {
+        let exp1 = (type == .decreasing) ? (yRange.value1 * (xRange.value2 - x)) : (yRange.value2 * (x - xRange.value1))
+        let exp2 = (type == .decreasing) ? (yRange.value2 * (xRange.value1 - x)) : (yRange.value1 * (x - xRange.value2))
+        let exp3 = (xRange.value2 - xRange.value1)
+        
+        return ((exp1 - exp2) / exp3)
+    }
+    
+    
+    
+    
+    // MARK: - UIScrollViewDelegate methods
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let xRange: RangeTuple = (0.0, self.scrollViewContentTopConstraint.constant)
+        
+        let yRangeForHeaderView: RangeTuple = (self.maxHeightForHeaderView, self.minHeightForHeaderView)
+        let newHeaderViewHeight = self.valueBetween(yRange: yRangeForHeaderView, forX: scrollView.contentOffset.y, between: xRange, functionType: .decreasing)
+        
+        let yRangeForBottomTitleLabel: RangeTuple = (self.maxBottomForTitleLabel, self.minBottomForTitleLabel)
+        let newTitleLabelBottom = self.valueBetween(yRange: yRangeForBottomTitleLabel, forX: scrollView.contentOffset.y, between: xRange, functionType: .decreasing)
+        
+        let yRangeForLeftTitleLabel: RangeTuple = (self.minLeftForTitleLabel, self.maxLeftForTitleLabel)
+        let newTitleLabelLeft = self.valueBetween(yRange: yRangeForLeftTitleLabel, forX: scrollView.contentOffset.y, between: xRange, functionType: .increasing)
+        
+        let yRangeForTitleLabelFontSize: RangeTuple = (self.maxTitleLabelFontSize, self.minTitleLabelFontSize)
+        let newTitleLabelFontSize = self.valueBetween(yRange: yRangeForTitleLabelFontSize, forX: scrollView.contentOffset.y, between: xRange, functionType: .decreasing)
+        
+        self.headerViewHeightConstraint.constant = (newHeaderViewHeight <= self.minHeightForHeaderView) ? self.minHeightForHeaderView : newHeaderViewHeight
+        self.titleLabelBottomConstraint.constant = (newTitleLabelBottom <= self.minBottomForTitleLabel) ? self.minBottomForTitleLabel : newTitleLabelBottom
+        self.titleLabelLeftConstraint.constant = (newTitleLabelLeft <= self.minLeftForTitleLabel) ? self.minLeftForTitleLabel : ((newTitleLabelLeft >= self.maxLeftForTitleLabel) ?  self.maxLeftForTitleLabel : newTitleLabelLeft)
+        self.titleLabel.font = self.titleLabel.font.withSize(newTitleLabelFontSize <= self.minTitleLabelFontSize ? self.minTitleLabelFontSize : ((newTitleLabelFontSize >= self.maxTitleLabelFontSize) ?  self.maxTitleLabelFontSize : newTitleLabelFontSize))
     }
     
 }
